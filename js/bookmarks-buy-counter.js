971982a6091268cf40ab302d1cf01eea5608c869
mbanakova <63152399+mbanakova@users.noreply.github.com>
2020-05-22 10:55:56 +0300

    4. Доступность и формы
    6. Продолжаем разметку

---
+++ js/bookmarks-buy-counter.js
@@ -0,0 +1,21 @@
+let bookmarks = document.querySelectorAll('.bookmark');
+let bookmarkNumber = document.querySelector('.bookmark-clicks');
+let bookmarkCounter = 0;
+
+for (bookmark of bookmarks) {
+bookmark.onclick = function () {
+    bookmarkCounter++;
+bookmarkNumber.textContent = bookmarkCounter;
+}
+}
+
+let buys = document.querySelectorAll('.buy');
+let buyNumber = document.querySelector('.buy-clicks');
+let buyCounter = 0;
+
+for (buy of buys) {
+buy.onclick = function () {
+    buyCounter++;
+buyNumber.textContent = buyCounter;
+}
+}
