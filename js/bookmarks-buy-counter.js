ef3a222c93ca601cc1b257facd5d090fcdcbadca
mbanakova <63152399+mbanakova@users.noreply.github.com>
2020-06-02 02:38:26 +0300

    7. Декоративные элементы

--- js/bookmarks-buy-counter.js
+++ js/bookmarks-buy-counter.js
@@ -1,26 +1,28 @@
 let bookmarks = document.querySelectorAll('.bookmark');
 let bookmarkNumber = document.querySelector('.bookmark-clicks');
 let bookmarkCounter = 0;
+let bookmarkTop = document.querySelector('.bookmarks');
 
 
 for (bookmark of bookmarks) {
     bookmark.onclick = function () {
     bookmarkCounter++;
     bookmarkNumber.textContent = bookmarkCounter;
+    bookmarkTop.style.backgroundColor = '#ee3643';
 }
 }
 
 let buys = document.querySelectorAll('.buy');
 let buyNumber = document.querySelector('.buy-clicks');
 let buyCounter = 0;
-let cart = document.querySelector('.cart');
+let cartTop = document.querySelector('.cart');
 
 
 for (buy of buys) {
     buy.onclick = function () {
     buyCounter++;
     buyNumber.textContent = buyCounter;
-    cart.style.backgroundColor = '#ee3643';
+    cartTop.style.backgroundColor = '#ee3643';
 }
 }
 
