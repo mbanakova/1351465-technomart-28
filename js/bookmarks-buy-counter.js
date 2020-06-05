a63bfc3f88f509df48e5606fa002b1d631a99e87
mbanakova <63152399+mbanakova@users.noreply.github.com>
2020-06-05 14:54:28 +0300

    поп-ап для корзинки

--- js/bookmarks-buy-counter.js
+++ js/bookmarks-buy-counter.js
@@ -17,13 +17,22 @@ let buyNumber = document.querySelector('.buy-clicks');
 let buyCounter = 0;
 let cartTop = document.querySelector('.cart');
 
+let cartPop = document.querySelector('.in-cart');
+let closeCart = document.querySelector('.pop-close.pop-cart');
+
 
 for (buy of buys) {
     buy.onclick = function () {
     buyCounter++;
     buyNumber.textContent = buyCounter;
     cartTop.style.backgroundColor = '#ee3643';
+    cartPop.classList.remove('hide');
 }
 }
 
+closeCart.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    cartPop.classList.remove('appear');
+    cartPop.classList.add('hide');
+});
 
